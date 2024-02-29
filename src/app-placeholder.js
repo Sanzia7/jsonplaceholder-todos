670363801427b04822411363cf0cf95afbe5d880
Svetlana M <meritee@gmx.de>
2024-02-29 08:49:28 +0100

    app simplification

--- src/app-placeholder.js
+++ src/app-placeholder.js
@@ -1,51 +1,55 @@
-import { useState } from 'react'
+import { useState} from 'react'
 import {
 	usePlaceholderGet,
-	usePlaceholderAdd,
-	usePlaceholderDelete,
-	usePlaceholderUpdate,
+	// usePlaceholderAdd,
+	// usePlaceholderDelete,
+	// usePlaceholderUpdate,
 } from './hooks-json-placeholder'
 
 import {TodoList} from './components'
-import {sortingTodos} from './utils'
+// import {sortingTodos} from './utils'
 import styles from './app.module.css'
 
 export const App = () => {
 	const [refreshTodosFlag, setRefreshTodosFlag] = useState(false)
-	const [isSorted, setIsSorted] = useState(false)
+	// const [isSorted, setIsSorted] = useState(false)
 
-	const refreshTodos = () => setRefreshTodosFlag(!refreshTodosFlag)
-
+	// const refreshTodos = () => setRefreshTodosFlag(!refreshTodosFlag)
+//
 	const {isLoading, todos} = usePlaceholderGet(refreshTodosFlag)
 
-	const sortedTodos = isSorted ? sortingTodos(todos) : todos
 
-	const { isCreating, requestAddTodo } = usePlaceholderAdd(refreshTodos)
 
-	const { isDeleting, requestDeleteTodo } = usePlaceholderDelete(refreshTodos)
 
-	const { isUpdating, requestUpdateTodos } = usePlaceholderUpdate(refreshTodos)
+	// const sortedTodos = isSorted ? sortingTodos(todos) : todos
+
+	// const { isCreating, requestAddTodo } = usePlaceholderAdd(refreshTodos)
+
+	// const { isDeleting, requestDeleteTodo } = usePlaceholderDelete(refreshTodos)
+//
+	// // const { isUpdating, requestUpdateTodos } = usePlaceholderUpdate(refreshTodos)
 
 	return (
 		<div className={styles.app}>
 			<h1>JSON Placeholder Todo App</h1>
 
-			<button onClick={() => setIsSorted(!isSorted)}>Sorting: A-B</button>
+			{/* <button onClick={() => setIsSorted(!isSorted)}>Sorting: A-B</button> */}
 
-			<button disabled={isCreating} onClick={requestAddTodo}>
-				Add new Todo
-			</button>
-			<button disabled={isDeleting} onClick={requestDeleteTodo}>
-				Remove
-			</button>
-			<button disabled={isUpdating} onClick={requestUpdateTodos}>
-				Update Todos
-			</button>
+			{/* <button disabled={isCreating} onClick={requestAddTodo}> */}
+				{/* Add new Todo */}
+			{/* </button> */}
+			{/* <button disabled={isDeleting} onClick={requestDeleteTodo}> */}
+				{/* Remove */}
+			{/* </button> */}
+			{/* <button disabled={isUpdating} onClick={requestUpdateTodos}> */}
+				{/* Update Todos */}
+			{/* </button> */}
 
 			{isLoading ? (
 				<div className={styles.loader}></div>
 			) : (
-				<TodoList todos={sortedTodos} />
+				// <TodoList todos={sortedTodos} />
+				<TodoList todos={todos} />
 			)}
 		</div>
 	)
--- src/hooks-json-placeholder/index.js
+++ src/hooks-json-placeholder/index.js
@@ -1,5 +1,5 @@
-export * from './use-placeholder-add'
+// export * from './use-placeholder-add'
 export * from './use-placeholder-get'
-export * from './use-placeholder-delete'
-export * from './use-placeholder-update'
+// export * from './use-placeholder-delete'
+// export * from './use-placeholder-update'
 
